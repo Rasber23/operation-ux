0da231b6cdd8d26e44fb8f1a4cfc6eb7651dc4d2
Helena <helena.theise@gmail.com>
2021-03-16 21:42:39 +0100

    <feat> added generic linechart

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,13 +1,67 @@
 <template>
-
+  <div>
+    <apexchart
+      :type="type"
+      width="550"
+      :options="chartOptions"
+      :series="series"
+    ></apexchart>
+    <button @click="updateChart">Update!</button>
+  </div>
 </template>
 
 <script>
+import VueApexCharts from "vue3-apexcharts";
+
 export default {
-  name: "LineChart"
-}
+  components: {
+    apexchart: VueApexCharts,
+  },
+
+  data: function () {
+    return {
+      type: "bar",
+      chartOptions: {
+        chart: {
+          id: "vuechart-example",
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+        },
+      },
+      series: [
+        {
+          name: "series-1",
+          data: [30, 40, 45, 50, 49, 60, 70, 81],
+        },
+      ],
+    };
+  },
+
+  methods: {
+    updateChart() {
+      const max = 90;
+      const min = 20;
+      const newData = this.series[0].data.map(() => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      });
+
+      const colors = ["#008FFB", "#00E396", "#FEB019", "#FF4560", "#775DD0"];
+
+      // Make sure to update the whole options config and not just a single property to allow the Vue watch catch the change.
+      this.chartOptions = {
+        colors: [colors[Math.floor(Math.random() * colors.length)]],
+      };
+
+      // In the same way, update the series option
+      this.series = [
+        {
+          data: newData,
+        },
+      ];
+      /** If i try to change the type i will get an error */
+      this.type = "line";
+    },
+  },
+};
 </script>
-
-<style scoped>
-
-</style>
