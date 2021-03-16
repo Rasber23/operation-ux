5b1e8e0d4b0d2c4fc5f55afc4649d2d701e822da
Helena <helena.theise@gmail.com>
2021-03-16 23:51:10 +0100

    <feat> added clickable legend to LineChart.vue

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -10,14 +10,15 @@
     <span v-for="work in workCount" :key="work.id">
       {{ work + " " }}
     </span>
-    <br /><br />
+    <br><br>
+    <div class="chart-wrapper">
     <apexchart
       :type="type"
       width="550"
       :options="chartOptions"
       :series="series"
     ></apexchart>
-    <button @click="updateChart">Update!</button>
+    </div>
   </div>
 </template>
 
@@ -29,21 +30,54 @@ export default {
     apexchart: VueApexCharts
   },
 
-  data: function () {
+  data() {
     return {
-      type: "bar",
+      type: "line",
       chartOptions: {
         chart: {
-          id: "vuechart-example"
+          id: "vuechart-example",
+          toolbar: { show: false }
         },
         xaxis: {
-          categories: [1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010]
+          categories: [
+            1920,
+            1930,
+            1940,
+            1950,
+            1960,
+            1970,
+            1980,
+            1990,
+            2000,
+            2010
+          ]
+        },
+        stroke: {
+          curve: "smooth"
+        },
+        colors: ["#ffd384", "#ffab73", "#ffaec0"],
+        legend: {
+          showForNullSeries: false,
+          showForZeroSeries: false,
+          position: "top",
+          fontSize: "25px",
+          onItemClick: {
+            toggleDataSeries: true
+          },
+          itemMargin: {
+            horizontal: 10,
+            vertical: 5
+          }
         }
       },
       series: [
         {
-          name: "series-1",
-          data: this.workCount
+          name: this.selected,
+          data: []
+        },
+        {
+          name: "garden",
+          data: [2, 4, 7, 4, 2, 778, 9, 9, 9]
         }
       ],
       years: [1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010],
@@ -64,16 +98,41 @@ export default {
         let subject = await resp.json();
         this.workCount.push(subject.work_count);
       }
-      return this.workCount;
+      this.updateChart();
+      // return this.workCount;
       // console.log(this.workCount);
     },
 
     // dubbelklick är ett problem här, hur prevent?
     clicked(event) {
       this.workCount.length = 0;
-      this.typeOfSubject = event.target.value;
+      this.selected = event.target.value;
       this.workCountForSubject();
     },
-  },
+    // kanske istället fylla i knappar och sen trycka uppdatera? enklare?
+    updateChart() {
+      this.series = [
+        {
+          name: this.selected,
+          data: this.workCount
+        },
+        {
+          name: "hello",
+          data: [45, 23, 53, 15, 6, 3, 5, 66, 2]
+        },
+        {
+          name: "lol",
+          data: [55, 42, 98, 89, 12, 1, 23]
+        }
+      ];
+    }
+  }
 };
 </script>
+<style scoped>
+div.chart-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
