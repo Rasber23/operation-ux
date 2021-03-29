9b7c5b72c30946cf63f4b2280bf6652de8df45c7
Lukas Blomdell <lukasblomdell@gmail.com>
2021-03-29 15:51:06 +0200

    <fix> sec. draft ChartMenu

--- src/components/ChartMenu.vue
+++ src/components/ChartMenu.vue
@@ -1,19 +1,92 @@
 <template>
-  <div class="linechart">
-    <div class="split left">
-      <button class="big-btn" value="linechart" @click="navigate">
-        linechart
-      </button>
-      <br/>
-      <button class="big-btn" value="barchart" @click="navigate">
-        barchart
-      </button>
-      <br/>
-      <button class="big-btn" value="bookofdeath" @click="navigate">
-        book of death
-      </button>
-    </div>
-  </div>
+  <nav class="navbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <router-link class="nav-link" to="/charts/linechart">
+        <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fad"
+            data-icon="cat"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            class="svg-inline--fa fa-cat fa-w-16 fa-9x"
+          >
+            <g class="fa-group">
+              <path
+                fill="currentColor"
+                d="M448 96h-64l-64-64v134.4a96 96 0 0 0 192 0V32zm-72 80a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm80 0a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm-165.41 16a204.07 204.07 0 0 0-34.59 2.89V272l-43.15-64.73a183.93 183.93 0 0 0-44.37 26.17L192 304l-60.94-30.47L128 272v-80a96.1 96.1 0 0 0-96-96 32 32 0 0 0 0 64 32 32 0 0 1 32 32v256a64.06 64.06 0 0 0 64 64h176a16 16 0 0 0 16-16v-16a32 32 0 0 0-32-32h-32l128-96v144a16 16 0 0 0 16 16h32a16 16 0 0 0 16-16V289.86a126.78 126.78 0 0 1-32 4.54c-61.81 0-113.52-44.05-125.41-102.4z"
+                class="fa-secondary"
+              ></path>
+              <path
+                fill="currentColor"
+                d="M376 144a16 16 0 1 0 16 16 16 16 0 0 0-16-16zm80 0a16 16 0 1 0 16 16 16 16 0 0 0-16-16zM131.06 273.53L192 304l-23.52-70.56a192.06 192.06 0 0 0-37.42 40.09zM256 272v-77.11a198.62 198.62 0 0 0-43.15 12.38z"
+                class="fa-primary"
+              ></path>
+            </g>
+          </svg>
+        <span class="link-text">Linechart</span>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/charts/barchart">
+        <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fad"
+            data-icon="cat"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            class="svg-inline--fa fa-cat fa-w-16 fa-9x"
+          >
+            <g class="fa-group">
+              <path
+                fill="currentColor"
+                d="M448 96h-64l-64-64v134.4a96 96 0 0 0 192 0V32zm-72 80a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm80 0a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm-165.41 16a204.07 204.07 0 0 0-34.59 2.89V272l-43.15-64.73a183.93 183.93 0 0 0-44.37 26.17L192 304l-60.94-30.47L128 272v-80a96.1 96.1 0 0 0-96-96 32 32 0 0 0 0 64 32 32 0 0 1 32 32v256a64.06 64.06 0 0 0 64 64h176a16 16 0 0 0 16-16v-16a32 32 0 0 0-32-32h-32l128-96v144a16 16 0 0 0 16 16h32a16 16 0 0 0 16-16V289.86a126.78 126.78 0 0 1-32 4.54c-61.81 0-113.52-44.05-125.41-102.4z"
+                class="fa-secondary"
+              ></path>
+              <path
+                fill="currentColor"
+                d="M376 144a16 16 0 1 0 16 16 16 16 0 0 0-16-16zm80 0a16 16 0 1 0 16 16 16 16 0 0 0-16-16zM131.06 273.53L192 304l-23.52-70.56a192.06 192.06 0 0 0-37.42 40.09zM256 272v-77.11a198.62 198.62 0 0 0-43.15 12.38z"
+                class="fa-primary"
+              ></path>
+            </g>
+          </svg>
+        <span class="link-text">Barchart</span>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/charts/bookofdeath">
+        <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fad"
+            data-icon="cat"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            class="svg-inline--fa fa-cat fa-w-16 fa-9x"
+          >
+            <g class="fa-group">
+              <path
+                fill="currentColor"
+                d="M448 96h-64l-64-64v134.4a96 96 0 0 0 192 0V32zm-72 80a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm80 0a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm-165.41 16a204.07 204.07 0 0 0-34.59 2.89V272l-43.15-64.73a183.93 183.93 0 0 0-44.37 26.17L192 304l-60.94-30.47L128 272v-80a96.1 96.1 0 0 0-96-96 32 32 0 0 0 0 64 32 32 0 0 1 32 32v256a64.06 64.06 0 0 0 64 64h176a16 16 0 0 0 16-16v-16a32 32 0 0 0-32-32h-32l128-96v144a16 16 0 0 0 16 16h32a16 16 0 0 0 16-16V289.86a126.78 126.78 0 0 1-32 4.54c-61.81 0-113.52-44.05-125.41-102.4z"
+                class="fa-secondary"
+              ></path>
+              <path
+                fill="currentColor"
+                d="M376 144a16 16 0 1 0 16 16 16 16 0 0 0-16-16zm80 0a16 16 0 1 0 16 16 16 16 0 0 0-16-16zM131.06 273.53L192 304l-23.52-70.56a192.06 192.06 0 0 0-37.42 40.09zM256 272v-77.11a198.62 198.62 0 0 0-43.15 12.38z"
+                class="fa-primary"
+              ></path>
+            </g>
+          </svg>
+        <span class="link-text">Book of Death</span>
+        </router-link>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
@@ -29,8 +102,61 @@ export default {
 
 
 <style scoped>
-.split {
+.navbar {
+  position: fixed;
+  background-color: #591716;
+  height: 90%;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  border-radius: 1rem 1rem;
+  overflow: hidden;
 
+  /*
+  skall inte vara här skall fixas med grid eller flexbox.
+  */
+  width: 15%;
+}
+
+.navbar-nav {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+}
+
+.nav-item {
+  width: 100%;
+}
+
+.nav-link {
+  display: flex;
+  align-items: center;
+  height: 5rem;
+  color: #FFFAF0;
+  text-decoration: none;
+}
+
+.nav-link:hover {
+  filter: grayscale(0%) opacity(1);
+  background: #6F1D1B;
+  color: #BBB9B9;
+}
+
+.link-text {
+  margin-left: 1rem;
+}
+
+.nav-link svg {
+  width: 2rem;
+  min-width: 2rem;
+  margin: 0 1.5rem;
+}
+
+.split {
+  background-color: #591716;
   margin: 0;
   height: 100%;
   width: 15%;
@@ -42,20 +168,10 @@ export default {
 .left {
   left: 0;
   padding-right: 50px;
-  background-color: #ffffff;
-}
-
-.right {
-  right: 0;
-  background-color: #9c9c9d;
-  width: 85%;
-  height: 100%;
-  overflow-x: hidden;
-  color: #ffffff;
 }
 
 .big-btn {
-  height: 250px;
+  height: 100px;
   width: 100%;
   background-color: #ffffff;
   transition-duration: 0.4s;
