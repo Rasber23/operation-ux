65004620861a0d51e8de4c09546742eee1fb6286
RasBER <rasmusbernestal@gmail.com>
2021-04-04 22:18:57 +0200

    ADded transactions

--- src/views/Charts.vue
+++ src/views/Charts.vue
@@ -1,17 +1,34 @@
 <template>
   <div>
     <chart-menu></chart-menu>
-    <div class="split right">
-      <router-view></router-view>
+    <div class="split right" @click="checkName">
+      <router-view v-slot="{ Component }">
+        <transition :name="test" >
+          <component :is="Component" />
+        </transition>
+      </router-view>
     </div>
   </div>
 </template>
 
 <script>
 import ChartMenu from "@/components/ChartMenu"
+
 export default {
   name: "Charts",
-  components: { ChartMenu },
+  components: {ChartMenu},
+  props:['transitionName','test'],
+  data() {
+    return {
+
+    }
+  },
+methods:{
+  checkName(){
+    console.log("TRANS NAME;"+ this.test)
+  }
+},
+
 }
 </script>
 <style scoped>
@@ -21,11 +38,77 @@ export default {
   position: fixed;
   padding-top: 20px;
 }
+
 .right {
   right: 0;
   background-color: #ffffff;
   width: 85%;
   height: 100%;
   overflow-x: hidden;
+}
+
+/*
+Route transitions ____up___
+ */
+
+.slide-up-enter-active,
+.slide-up-leave-active {
+  transition: all 0.5s ease-out;
+}
+
+
+.slide-up-enter-to {
+  position: absolute;
+  top: 0;
+}
+
+
+.slide-up-enter-from {
+  position: absolute;
+  top: -100%;
+}
+
+
+.slide-up-leave-to {
+  position: absolute;
+  top: -100%;
+}
+
+
+.slide-up-leave-from {
+  position: absolute;
+  top: 0;
+}
+
+/*
+   ::::: ___down___ :::::
+ */
+.slide-down-enter-active,
+.slide-down-leave-active {
+  transition: all 0.5s ease-out;
+}
+
+
+.slide-down-enter-to {
+  position: absolute;
+  right: 0;
+}
+
+
+.slide-down-enter-from {
+  position: absolute;
+  right: -100%;
+}
+
+
+.slide-down-leave-to {
+  position: absolute;
+  right: -100%;
+}
+
+
+.slide-down-leave-from {
+  position: absolute;
+  right: 0;
 }
 </style>
