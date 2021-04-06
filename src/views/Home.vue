5b1102a9f1655b2fcb998461947f7ad55a6e31f4
RasBER <rasmusbernestal@gmail.com>
2021-04-06 22:40:40 +0200

    added Nav buttons to home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,6 +25,17 @@
       <div class="col-12 col-lg-6">
         <img src="https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2053&q=80" class="img-fluid rounded" alt="...">
       </div>
+      <div id="navCON">
+        <router-link to="/charts/linechart">
+          <img class="homeNav" src="../assets/Icons/Graph.svg" alt=""   />
+        </router-link>
+        <router-link to="/charts/barchart">
+          <img class="homeNav" src="../assets/Icons/Bar_chart.svg" alt=""   />
+        </router-link>
+        <router-link to="/charts/bookOfDeath">
+          <img class="homeNav" src="../assets/Icons/Falling_book.svg" alt=""   />
+        </router-link>
+      </div>
     </div>
   </div>
 
@@ -73,5 +84,18 @@ export default {
   height: 100px;
   margin: 10em;
 }
+#navCON{
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  margin-top: 1em;
+  margin-bot: 2em;
 
+}
+.homeNav{
+  height: 5em;
+  width: 5em;
+  background: #6F1D1B;
+  border-radius: 3px;
+}
 </style>
