08c3449616ae0ad1f764dba903809fd4f281fcac
Lukas Blomdell <lukasblomdell@gmail.com>
2021-04-07 14:05:23 +0200

    <fix> Fact svgs

--- src/components/Facts.vue
+++ src/components/Facts.vue
@@ -1,25 +1,37 @@
 <template>
   <div id="fact-modal" v-if="!isMobile || showModal">
-      <div class="modal-wrapper">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-body">
-              <h2><slot name="heading"></slot></h2>
-              <h5><slot name="subheading"></slot></h5>
-              <p><slot name="text"></slot></p>
-              <p v-if="show">width: {{ width }}</p>
-            </div>
-            <div v-if="isMobile" class="modal-footer">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true" @click="showModal = false">&times;</span>
-              </button>
+    <div class="modal-wrapper">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-body">
+            <h2><slot name="heading"></slot></h2>
+            <h5><slot name="subheading"></slot></h5>
+            <p><slot name="text"></slot></p>
+            <p v-if="show">width: {{ width }}</p>
+          </div>
+          <div v-if="isMobile" class="modal-footer">
+            <div class="close" data-dismiss="modal" aria-label="Close">
+              <img
+                aria-hidden="true"
+                @click="showModal = false"
+                class="paddingForSvg"
+                src="./../assets/Icons/Close_meny_black.svg"
+                alt=""
+              />
             </div>
           </div>
         </div>
       </div>
     </div>
-  <div v-if="isMobile">
-    <button v-if="!showModal" @click="showModal = true">Click</button>
+  </div>
+  <div class="d-flex justify-content-end flex-row" v-if="isMobile">
+    <img
+      v-if="!showModal"
+      @click="showModal = true"
+      class="paddingForSvg"
+      src="./../assets/Icons/question-circle-regular.svg"
+      alt=""
+    />
   </div>
 </template>
 
@@ -42,14 +54,12 @@ export default {
     },
     addMask() {
       // create a new div element
-     // const mask = document.createElement("div")
-     // mask.className = 'modal-mask'
-
+      // const mask = document.createElement("div")
+      // mask.className = 'modal-mask'
       // add the newly created element and its content into the DOM
       //document.querySelector("fact-modal").appendChild(mask)
     },
-    removeMask() {
-    }
+    removeMask() {},
   },
   watch: {
     width: function () {
@@ -77,6 +87,10 @@ export default {
   background-color: rgba(0, 0, 0, 0.5);
   display: table;
   transition: opacity 0.3s ease;
+}
+
+.paddingForSvg {
+  padding: 1em;
 }
 
 .modal-dialog {
