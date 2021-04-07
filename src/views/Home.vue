4915d7aa8393dd6b41e7e49cf5f31f439bcec677
RasBER <rasmusbernestal@gmail.com>
2021-04-07 13:07:56 +0200

    fixed home page!

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,25 +9,20 @@
         </h5>
         <br>
         <h5>Låt oss hjälpa dig upptäcka böckernas värld! :3</h5>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur cum enim exercitationem expedita
-          explicabo inventore iure magni mollitia obcaecati praesentium, sequi, sint tempora ullam, voluptatum.
-          Consequatur cum deserunt dolor eos ex quam quod sapiente sunt voluptatem? Dolore eveniet quod soluta. Ab
-          aliquam aliquid animi asperiores illum labore quam reprehenderit tempora voluptates. Cupiditate, et fuga
-        </p>
+
       </div>
       <div class="col-12 col-lg-6">
         <img src="https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2053&q=80" class="img-fluid rounded" alt="...">
       </div>
       <div id="navCON">
         <router-link to="/charts/linechart">
-          <img class="homeNav" src="../assets/Icons/Graph.svg" alt=""   />
+          <img class="homeNav" src="../assets/Icons/Graph.svg" alt=""/>
         </router-link>
         <router-link to="/charts/barchart">
-          <img class="homeNav" src="../assets/Icons/Bar_chart.svg" alt=""   />
+          <img class="homeNav" src="../assets/Icons/Bar_chart.svg" alt=""/>
         </router-link>
         <router-link to="/charts/bookOfDeath">
-          <img class="homeNav" src="../assets/Icons/Falling_book.svg" alt=""   />
+          <img class="homeNav" src="../assets/Icons/Falling_book.svg" alt=""/>
         </router-link>
       </div>
     </div>
